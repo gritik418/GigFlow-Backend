2b8af0c4a7f0e7b743a1e34b99f7eccd0cd13615
Ritik <160255700+gritik418@users.noreply.github.com>
2026-01-14 12:19:46 +0530

    chore: updated redis config

--- src/queues/email.queue.ts
+++ src/queues/email.queue.ts
@@ -3,10 +3,10 @@ import { Queue } from "bullmq";
 import Redis from "ioredis";
 
 // @ts-ignore
-export const connection = new Redis({
-  url: process.env.REDIS_URL!,
-  maxRetriesPerRequest: null,
-});
+export const connection = new Redis(process.env.REDIS_URL!);
+
+connection.on("connect", () => console.log("✅ Redis connected!"));
+connection.on("error", (err: any) => console.log(`❌ Redis error: `, err));
 
 const emailQueue = new Queue("email-queue", {
   connection,
