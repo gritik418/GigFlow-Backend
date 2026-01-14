cfbfcea520c78901141448776c10e6f83954f73a
Ritik <160255700+gritik418@users.noreply.github.com>
2026-01-14 13:57:10 +0530

    chore: updated cors policy

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: process.env.CLIENT_BASE_URL,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
